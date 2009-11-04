7f6805d2f3efff93afdcc6d83bfc546bbc703858
Gregory Man <man.gregory@gmail.com>
2009-11-04 23:11:45 +0200

    Added hoptoad_notification option to capistrano recipe

--- recipes/hoptoad.rb
+++ recipes/hoptoad.rb
@@ -5,18 +5,25 @@
 # to your deploy.rb
 #
 # Defines deploy:notify_hoptoad which will send information about the deploy to Hoptoad.
+# Use "set :hoptoad_notification, false" in your config, to not notify Hoptoad (usful for staging envs).
 #
+_cset :hoptoad_notification, true
+
 after "deploy",            "deploy:notify_hoptoad"
 after "deploy:migrations", "deploy:notify_hoptoad"
 
 namespace :deploy do
   desc "Notify Hoptoad of the deployment"
   task :notify_hoptoad do
-    rails_env = fetch(:rails_env, "production")
-    local_user = ENV['USER'] || ENV['USERNAME']
-    notify_command = "rake hoptoad:deploy TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
-    puts "Notifying Hoptoad of Deploy (#{notify_command})"
-    `#{notify_command}`
-    puts "Hoptoad Notification Complete."
+    if hoptoad_notification
+      rails_env = fetch(:rails_env, "production")
+      local_user = ENV['USER'] || ENV['USERNAME']
+      notify_command = "rake hoptoad:deploy TO=#{rails_env} REVISION=#{current_revision} REPO=#{repository} USER=#{local_user}"
+      puts "Notifying Hoptoad of Deploy (#{notify_command})"
+      `#{notify_command}`
+      puts "Hoptoad Notification Complete."
+    else
+      puts "Not notifying Hoptoad"
+    end
   end
 end
